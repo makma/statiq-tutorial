39baedce1481d46119bbb2217f55e015ceeddffb
Martin Makarsky <martinmakarsky@gmail.com>
2020-12-01 15:03:51 +0100

    feat: unify accessing environment variables and appsettings.json for initing DeliveryOptions

--- Program.cs
+++ Program.cs
@@ -9,18 +9,23 @@ using Statiq.Web;
 
 namespace StatiqTutorial
 {
-  public class Program
-  {
-    public static async Task<int> Main(string[] args) =>
-      await Bootstrapper
-        .Factory
-        .CreateDefault(args) // Creates a bootstrapper with a default configuration including logging, commands, shortcodes, and assembly scanning. Disables automatic creating of html pages from markdonw files. Fixes https://github.com/statiqdev/Statiq.Framework/issues/140
-        .ConfigureServices((services, settings) =>
-        {
-            services.AddSingleton<ITypeProvider, CustomTypeProvider>();
-            services.AddDeliveryClient((IConfiguration)settings);
-        })
+    public class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+          await Bootstrapper
+            .Factory
+            .CreateDefault(args) // Creates a bootstrapper with a default configuration including logging, commands, shortcodes, and assembly scanning. Disables automatic creating of html pages from markdonw files. Fixes https://github.com/statiqdev/Statiq.Framework/issues/140
+            .ConfigureServices((services, settings) =>
+            {
+                DeliveryOptions deliveryOptions = new DeliveryOptions();
+                deliveryOptions.ProjectId = settings.GetString("ProjectId");
+                deliveryOptions.PreviewApiKey = !string.IsNullOrEmpty(settings.GetString("PreviewApiKey")) ? settings.GetString("PreviewApiKey") : null;
+                deliveryOptions.UsePreviewApi = settings.GetBool("UsePreviewApi");
+
+                services.AddSingleton<ITypeProvider, CustomTypeProvider>();
+                services.AddDeliveryClient(deliveryOptions);
+            })
         .AddHostingCommands() // enables preview pipeline
         .RunAsync();
-  }
+    }
 }
